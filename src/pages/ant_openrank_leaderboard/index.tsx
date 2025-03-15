a92db6973645d515233723ba4e71af973e7da562
八鹤 <tangyenan.tyn@alibaba-inc.com>
2025-03-15 12:12:37 +0800

    feat: introduce ant-openrank-leaderboard via iframe

--- src/pages/ant_openrank_leaderboard/index.tsx
+++ src/pages/ant_openrank_leaderboard/index.tsx
@@ -1,10 +1,12 @@
 import Layout from '@theme/Layout';
 
+import styles from './index.module.css'
+
 export default (): JSX.Element => {
   return (
     <div className='no-footer'>
       <Layout>
-      xx
+        <iframe className={styles.container} src="https://tyn1998.github.io/ant-openrank-leaderboard/" />
       </Layout>
     </div>
   );
